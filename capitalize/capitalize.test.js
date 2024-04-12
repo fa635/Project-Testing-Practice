d4ef4be3562d0ac206ad162a0ee9175f44401829
fa635 <bfalonne823@gmail.com>
2024-04-12 09:23:48 +0200

    Create capitalize test

---
+++ capitalize/capitalize.test.js
@@ -0,0 +1,14 @@
+const capitalize = require("./capitalize/capitalize")
+
+
+describe("capitalize", () => {
+    test("capitalizes first character of string", () => {
+        expect(capitalize("hello")).toBe("Hello")
+    })
+    test("capitalizes first character of string", () => {
+        expect(capitalize("hey")).toBe("Hey")
+    })
+    test("capitalizes first character of string", () => {
+        expect(capitalize("fire")).toBe("Fire")
+    })
+})
